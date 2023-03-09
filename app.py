61ec038661c9d2ef4bdb9971abe39c3e3fc653c7
Sonu Gupta <109720276+SonuGupta00sg@users.noreply.github.com>
2023-03-09 11:26:40 +0000

    added some files

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Logos',
+    
+  },
+  {
+    'id': 2,
+    'title': 'Designs',
+    
+  },
+  {
+    'id': 3,
+    'title': 'Drawing',
+    
+  },
+  {
+    'id': 4,
+    'title': 'Emotes',
+    
+  }
+]
+
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='The Creaphics')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
-  app.run(host = "0.0.0.0", debug = True)
+  app.run(host='0.0.0.0', debug=True)
