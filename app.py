5cb087b8211447d06e47d40979ec91139daa1e9c
Sonu Gupta <109720276+SonuGupta00sg@users.noreply.github.com>
2023-05-03 09:59:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,11 @@ def hello_jovian():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+  
+@app.after_request
+def add_vary_cookie_header(response):
+    response.headers["Vary"] = "Cookie"
+    return response  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
